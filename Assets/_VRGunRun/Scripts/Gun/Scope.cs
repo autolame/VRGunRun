1ba19ab5d685cd768c0fb2d6d4807625414ba8b7
Kien <autolame@yahoo.de>
2017-07-25 16:33:40 +0200

    ak scope fix

--- Assets/_VRGunRun/Scripts/Gun/Scope.cs
+++ Assets/_VRGunRun/Scripts/Gun/Scope.cs
@@ -6,15 +6,21 @@ public class Scope : MonoBehaviour
 {
     public Camera RTCamera;
     public Renderer RenderTarget;
-
     public RenderTexture RenderTexture;
 
     public float MinZoomFOV = 10;
     public float MaxZoomFOV = 1;
 
+    private void Awake()
+    {
+        RenderTexture = new RenderTexture(2048, 2048, 0);
+        RenderTexture.Create();
+    }
 
     private void LateUpdate()
     {
-        RenderTexture = RTCamera.targetTexture;
+        RTCamera.targetTexture = RenderTexture;
+        RenderTarget.material.SetTexture("_MainTex", RenderTexture);
+        RenderTarget.material.SetTexture("_EmissionMap", RenderTexture);
     }
 }
