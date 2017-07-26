2011ce8b5a0b959b20d238acbcb28fcf3991c821
Kien <autolame@yahoo.de>
2017-07-26 19:04:26 +0200

    turret finds nearest enemy in range

--- Assets/_VRGunRun/Scripts/DefendingZones/Turret.cs
+++ Assets/_VRGunRun/Scripts/DefendingZones/Turret.cs
@@ -4,36 +4,83 @@ using UnityEngine;
 
 public class Turret : MonoBehaviour
 {
+    [SerializeField] private Transform turretHead;
+
     public float StartHitPoint = 2500f;
-    public List<Enemy> EnemiesInRange = new List<Enemy>();
+    public List<EnemyDrone> DronesInRange = new List<EnemyDrone>();
 
+    public float RotationSpeed = 1;
     public Enemy nearestEnemy;
+    float nearestEnemyDistance;
+    private RaycastHit raycastHit;
 
-    private void OnCollisionEnter(Collision collision)
+
+    private void OnTriggerEnter(Collider other)
     {
-        if (collision.gameObject.GetComponent<Enemy>())
+        if (other.GetComponent<EnemyDrone>())
         {
-            var enemy = collision.gameObject.GetComponent<Enemy>();
-            EnemiesInRange.Add(enemy);
+            var drone = other.GetComponent<EnemyDrone>();
+            DronesInRange.Add(drone);
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+
+        // get nearest enemy
+        foreach (var drone in DronesInRange)
+        {
+            if (drone == null)
+            {
+                DronesInRange.Remove(drone);
+            }
+
+            var droneDistance = Vector3.Distance(turretHead.transform.position, drone.transform.position);
+            if (droneDistance < nearestEnemyDistance)
+            {
+                nearestEnemyDistance = droneDistance;
+                nearestEnemy = drone;
+            }
+            else
+            {
+                nearestEnemyDistance = 20;
+            }
         }
 
-        foreach (var enemy in EnemiesInRange)
+        if (nearestEnemy)
         {
-            if (enemy == null)
+            SmoothLookAtPosition(nearestEnemy.transform.position, RotationSpeed);
+            bool onEnemy = Physics.Raycast(turretHead.transform.position, transform.forward, out raycastHit);
+            if (raycastHit.transform.GetComponent<EnemyDrone>())
             {
-                EnemiesInRange.Remove(enemy);
+                Shoot();
             }
         }
     }
 
-    private void Update()
+    private void OnTriggerExit(Collider other)
     {
-        foreach (var enemy in EnemiesInRange)
+        if (other.GetComponent<EnemyDrone>())
         {
-            if (enemy != null)
-            {
-                // TODO implement
-            }
+            var drone = other.GetComponent<EnemyDrone>();
+            DronesInRange.Remove(drone);
         }
+    }
+
+    void SmoothLookAtPosition(Vector3 position, float speed)
+    {
+        var targetRotation = Quaternion.LookRotation(position - turretHead.transform.position);
+        turretHead.transform.rotation = Quaternion.Slerp(turretHead.transform.rotation, targetRotation, speed * Time.deltaTime);
+    }
+
+    EnemyDrone NearestEnemy(EnemyDrone enemy)
+    {
+        var enemyDistance = Vector3.Distance(turretHead.transform.position, enemy.transform.position);
+        return enemy;
+    }
+
+    void Shoot()
+    {
+        // TODO shoot at target!
     }
 }
