21b916a45143aec4ae1404e4a4511eb4c23619ce
Kien <autolame@yahoo.de>
2017-08-01 18:21:41 +0200

    main menu

---
+++ Assets/_VRGunRun/Scripts/GUI/ShootMenuButton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ShootMenuButton : MonoBehaviour
+{
+    [SerializeField] private TextMeshPro buttonText;
+    private string menu;
+    private bool activated = false;
+    private ShootMenuManager menuManager;
+
+    public string Menu
+    { get { return menu; } }
+
+    private void Awake()
+    {
+        menu = buttonText.text;
+        menuManager = transform.root.GetComponent<ShootMenuManager>();
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.GetComponent<GunAmmoBullet>())
+        {
+            menuManager.ActivateMenu(menu, name);
+        }
+    }
+}
---
+++ Assets/_VRGunRun/Scripts/GUI/ShootMenuManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShootMenuManager : MonoBehaviour
+{
+    public void ActivateMenu(string menu, string objectName)
+    {
+        switch (menu)
+        {
+            case "Start":
+                NewGame();
+                break;
+            case "Quit":
+                ExitGame();
+                break;
+            case "Load":
+                LoadGame();
+                break;
+            case "":
+                Debug.LogError("Set up proper name for : " + objectName);
+                break;
+            default:
+                Debug.LogError("Incorrect Selection on :" + objectName);
+                break;
+        }
+    }
+
+    void NewGame()
+    {
+        Application.LoadLevel(0);
+        Application.UnloadLevel(-1);
+    }
+
+    void LoadGame()
+    {
+
+    }
+
+    void ExitGame()
+    {
+        Application.Quit();
+    }
+
+
+}
